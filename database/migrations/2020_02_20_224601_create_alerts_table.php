beddda6fca0da700bc49fb6828740fcd000a1056
Helder Correia <tuxboy@tlab.pt>
2020-02-20 23:00:38 +0000

    [TASK] Add events and sensors tables

---
+++ database/migrations/2020_02_20_224601_create_alerts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sensor_id')->unsigned();
+            $table->timestamp('alert_on')->nullable();
+            $table->timestamp('reset_on')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alert');
+    }
+}
