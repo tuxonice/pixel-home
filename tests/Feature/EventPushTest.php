a580b46cc7bde9035590513604b2325a994a602e
Helder Correia <tuxboy@tlab.pt>
2020-03-04 22:11:26 +0000

    Add tests

--- tests/Feature/EventPushTest.php
+++ tests/Feature/EventPushTest.php
@@ -12,17 +12,42 @@ class EventPushTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * A basic test example.
+     * Can push sensor data
      *
      * @return void
      */
     public function testCanPushEvents()
     {
-
         factory(Sensor::class)->create();
 
         $response = $this->get('/event/push/12345?sensor=HT01&hum=79&temp=17.00');
         $response->assertStatus(200);
+    }
+    
+    /**
+     * Can't push sensor data if hash is wrong
+     *
+     * @return void
+     */
+    public function testCanNotPushEventsWithWrongHashCode()
+    {
+        factory(Sensor::class)->create();
+
+        $response = $this->get('/event/push/abcd?sensor=HT01&hum=79&temp=17.00');
+        $response->assertStatus(401);
+    }
+    
+    /**
+     * Can't push sensor data if sensor code does not exist
+     *
+     * @return void
+     */
+    public function testCanNotPushEventsWithWrongSensorCode()
+    {
+        factory(Sensor::class)->create();
+
+        $response = $this->get('/event/push/12345?sensor=HT02&hum=79&temp=17.00');
+        $response->assertStatus(401);
     }
 }
 
