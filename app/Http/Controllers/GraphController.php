ceea92e6ca8a22c22f1118e2378c79200ac7c435
Helder Correia <tuxboy@tlab.pt>
2020-04-25 16:47:07 +0100

    [FIX] Show date/time on current user timezone

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Sensor;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class GraphController extends Controller
 {
@@ -42,6 +44,15 @@ class GraphController extends Controller
         ->select('events.*','sensors.name AS sensorName')
         ->join('sensors', 'events.sensor_id', '=', 'sensors.id')
         ->orderBy('added_on', 'asc')->get();
+        
+        $userTimezone = Auth::user()->timezone;
+
+        $events->map(function ($item, $key) use($userTimezone){
+            $item->added_on = Carbon::createFromFormat('Y-m-d H:i:s', $item->added_on, timezone_open('UTC'))
+                ->setTimezone($userTimezone)
+                ->toDateTimeString();
+            return $item;
+        });
         
         $grouped = $events->mapToGroups(function ($item, $key) {
             return [$item->sensorName => $item];
