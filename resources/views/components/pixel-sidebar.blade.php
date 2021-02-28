6202478de770eb855d419050126624e74afc959a
Helder Correia <tuxboy@tlab.pt>
2021-02-28 10:02:17 +0000

    [TASK] Add migrations

--- resources/views/components/pixel-sidebar.blade.php
+++ resources/views/components/pixel-sidebar.blade.php
@@ -3,12 +3,13 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Pixel Home</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
+      <!--
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
@@ -17,8 +18,9 @@
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
       </div>
-
+      -->
       <!-- SidebarSearch Form -->
+      <!--
       <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
           <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
@@ -29,17 +31,17 @@
           </div>
         </div>
       </div>
-
+      -->
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+          <li class="nav-item">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+                Devices
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
@@ -47,25 +49,62 @@
               <li class="nav-item">
                 <a href="#" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>List</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>New</p>
                 </a>
               </li>
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-thermometer-half"></i>
               <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
+                Sensors
+                <i class="right fas fa-angle-left"></i>
               </p>
             </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>List</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>New</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-chart-bar"></i>
+              <p>
+                Statistics
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data points</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Graph</p>
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
