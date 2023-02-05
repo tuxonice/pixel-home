5df2efd68c1b0712fed7225b2010115ded387f04
Helder Correia <tuxboy@tlab.pt>
2023-02-05 21:13:43 +0000

    [TASK] Lock AMCharts to version 5.3.2

--- resources/views/partials/graph/show.blade.php
+++ resources/views/partials/graph/show.blade.php
@@ -180,9 +180,9 @@
 @stop
 
 @section('js')
-<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-<script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
-<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+<script src="https://cdn.amcharts.com/lib/version/5.3.2/index.js"></script>
+<script src="https://cdn.amcharts.com/lib/version/5.3.2/xy.js"></script>
+<script src="https://cdn.amcharts.com/lib/version/5.3.2/themes/Animated.js"></script>
 
 <script src="{{ asset('js/moment.min.js') }}"></script>
 <script src="{{ asset('js/daterangepicker.js') }}"></script>
